df36daa369550026161495e3f85e7e13188f6585
kanakanho <kahoshibakichi@gmail.com>
2024-05-19 10:28:32 +0900

    remove: パーミッション

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 
 export function App() {
     const [alpha, setAlpha] = useState<number>(0);
-    const [permissionGranted, setPermissionGranted] = useState(false);
 
     useEffect(() => {
         const handleOrientationEvent = (event: DeviceOrientationEvent) => {
@@ -12,35 +11,13 @@ export function App() {
             }
         };
 
-        if (permissionGranted) {
-            window.addEventListener("deviceorientation", handleOrientationEvent);
-        }
-
         return () => {
             window.removeEventListener("deviceorientation", handleOrientationEvent);
         };
-    }, [permissionGranted]);
+    }, []);
 
     return (
         <div>
-            <button
-                onClick={() => {
-                    const deviceOrientationEvent = DeviceOrientationEvent as any;
-                    if (typeof deviceOrientationEvent.requestPermission === "function") {
-                        deviceOrientationEvent.requestPermission().then((response: string) => {
-                            if (response === "granted") {
-                                setPermissionGranted(true);
-                            } else {
-                                console.error("Permission not granted for Device Orientation");
-                            }
-                        });
-                    } else {
-                        setPermissionGranted(true); // Androidや非iOSデバイスの場合
-                    }
-                }}
-            >
-                Request permission
-            </button>
             <h1>Alpha:{alpha}</h1>
         </div>
     );
