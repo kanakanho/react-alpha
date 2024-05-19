51c3ebb8057446c7793edb15159bf2429e309d68
kanakanho <kahoshibakichi@gmail.com>
2024-05-19 10:36:26 +0900

    add: repoName

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// GitHub Pagesのリポジトリ名をここに設定します
+const repoName = 'react-alpha';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`
 })
